7d7abefd45a3ae74796859351adf084261f99a7e
Atul Das <Atul.Das1@Veritas.com>
2024-03-03 21:27:36 +0530

    updated the ps command

--- wincmds/alias_ps.cmd
+++ wincmds/alias_ps.cmd
@@ -4,6 +4,7 @@
 @DOSKEY .ps=powershell.exe $*
 @DOSKEY .ps1=powershell.exe -File $*
 @DOSKEY ps.unc=powershell.exe get-WmiObject -class Win32_Share -computer $*
+@DOSKEY ps.drvinfo=powershell.exe "get-wmiobject win32_logicaldisk |Format-Table Name,FileSystem,VolumeName,@{L='Size';E={[math]::truncate($_.freespace / 1GB)}}"
 
 :QA_ALIAS_PS
 
