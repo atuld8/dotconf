2eba357f244f7a32045574b12f2fd5d588c0e844
Atul Das <Atul.Das1@Veritas.com>
2024-08-14 12:16:53 +0530

    Feat: Add support to accept append string, release name, version details through command option

--- jira/j.createSubTasks.sh
+++ jira/j.createSubTasks.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# This script will create 3 sub-tasks under the given Jira Id
+# 1. Development Task:
+# 2. Documentation Task:
+# 3. QA Task:
 #
+# I/P = Jira_ID Release_label Release_version [componentName] [Any append string"
+# Ex: script JIRA-1234 "" 2.5 "" "Add support for option help"
+#
+# Hidden flag -d
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+DEBUG_MODE=0
+
+if [ "$1" == "-d" ]; then
+    set -x
+    DEBUG_MODE=1
+    shift
+fi
 
 JIRA_ID=$1;
-JIRA_RELEASE=$2
-JIRA_COMPONENT_NAME="${3:-Commandos}"
+JIRA_RELEASE_NAME=${2:-NBServerMigrator}
+JIRA_RELEASE_VER=$3
+JIRA_COMPONENT_NAME="${4:-Commandos}"
+JIRA_APPEND_STRING_TO_SUBTASK="$5"
 JIRA_COMPONENT_ID=""
 
 #
@@ -36,28 +56,28 @@ generatePostDataToCreateSubTasks() {
         {"fields": {
              "project": {"key": "NBU"},
              "parent": {"key": "$JIRA_ID"},
-             "summary": "Development Task",
+             "summary": "Development Task: ${JIRA_APPEND_STRING_TO_SUBTASK}",
              "description": "Keep track of all coding work, including new features and bug fixes.",
              "issuetype": {"id": "5"},
-             "labels": ["NBServerMigrator","NBServerMigrator_$JIRA_RELEASE"],
+             "labels": ["${JIRA_RELEASE_NAME}","${JIRA_RELEASE_NAME}_$JIRA_RELEASE_VER"],
              "components": [ {"id" : "$JIRA_COMPONENT_ID"} ]
          } },
          {"fields": {
              "project": {"key": "NBU"},
              "parent": {"key": "$JIRA_ID"},
-             "summary": "Documentation Task",
+             "summary": "Documentation Task: ${JIRA_APPEND_STRING_TO_SUBTASK}",
              "description": "Update documents to reflect the latest changes and improvements.",
              "issuetype": {"id": "5"},
-             "labels": ["NBServerMigrator","NBServerMigrator_$JIRA_RELEASE"],
+             "labels": ["${JIRA_RELEASE_NAME}","${JIRA_RELEASE_NAME}_$JIRA_RELEASE_VER"],
              "components": [ {"id" : "$JIRA_COMPONENT_ID"} ]
          } },
          {"fields": {
              "project": {"key": "NBU"},
              "parent": {"key": "$JIRA_ID"},
-             "summary": "QA Task",
+             "summary": "QA Task: ${JIRA_APPEND_STRING_TO_SUBTASK}",
              "description": "Follow and manage all testing activities to ensure everything works correctly.",
              "issuetype": {"id": "5"},
-             "labels": ["NBServerMigrator","NBServerMigrator_$JIRA_RELEASE"],
+             "labels": ["${JIRA_RELEASE_NAME}","${JIRA_RELEASE_NAME}_$JIRA_RELEASE_VER"],
              "components": [ {"id" : "$JIRA_COMPONENT_ID"} ]
          } }
       ]}
@@ -113,16 +133,20 @@ jira_post_call() {
    REST_GET_DATA_FUNC=$@;
 
 
-   sub_task_ids=(`echo "curl --silent -X POST \
-        -H \"Authorization: Bearer $JIRA_ACC_TOKEN\" \
-        -H \"Content-Type:application/json\" \
-        \"https://$JIRA_SERVER_NAME/rest/api/2/$REST_API_PATH\" \
-        --data-raw '"$($REST_GET_DATA_FUNC)"'" | sh | jq -r ".issues[].key"`)
+   if [ "$DEBUG_MODE" -eq 0 ]; then
+       sub_task_ids=(`echo "curl --silent -X POST \
+            -H \"Authorization: Bearer $JIRA_ACC_TOKEN\" \
+            -H \"Content-Type:application/json\" \
+            \"https://$JIRA_SERVER_NAME/rest/api/2/$REST_API_PATH\" \
+            --data-raw '"$($REST_GET_DATA_FUNC)"'" | sh | jq -r ".issues[].key"`)
 
-    echo "Total ${#sub_task_ids[@]} created."
-    for sub_task_id in ${sub_task_ids[@]}; do
-        echo $sub_task_id
-    done
+        echo "Total ${#sub_task_ids[@]} created."
+        for sub_task_id in ${sub_task_ids[@]}; do
+            echo $sub_task_id
+        done
+   else
+       echo "--data-raw $($REST_GET_DATA_FUNC)"
+   fi
         # set_component_id_to_jira_ticket $sub_task_id
 
 }
@@ -149,12 +173,12 @@ if [ "$JIRA_ID" == "" ]; then
 fi
 
 if [ "$JIRA_ID" == "-h" ]; then
-    echo "$0 JIRA_ID RELEASE_LABEL [TEAM_NAME]"
+    echo "$0 JIRA_ID RELEASE_NAME RELEASE_VERSION [TEAM_NAME]"
     exit 0
 fi
 
-if [ "$JIRA_RELEASE" == "" ]; then
-    echo "Please pass the JIRA_RELEASE with this script"
+if [ "$JIRA_RELEASE_VER" == "" ]; then
+    echo "Please pass the RELEASE_VER with this script"
     exit 1
 fi
 
