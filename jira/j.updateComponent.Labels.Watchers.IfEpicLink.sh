ca764945eaadd4cc17ba30d5918125bff8f48319
Atul Das <Atul.Das1@Veritas.com>
2024-09-03 12:02:54 +0530

    Feat: Added support to update Epic link if defined as env variable JIRA_EPIC_LINK

--- jira/j.updateComponent.Labels.Watchers.IfEpicLink.sh
+++ jira/j.updateComponent.Labels.Watchers.IfEpicLink.sh
@@ -8,6 +8,8 @@
 #export JIRA_SERVER_NAME=".com"
 #export JIRA_PROJECT_NAME="PROJ"
 #export JIRA_LABELS="Label1 Label2"
+#export JIRA_EPIC_LINK="EPIC-1234"
+
 
 JIRA_ID=$1
 JIRA_COMPONENT_NAME="${2:-Commandos}"
@@ -55,16 +57,37 @@ POST_DATA_EOF
 # Function
 #
 generatePostDataToUpdateWatcherGroupField() {
+  _JIRA_FIELD_ID=$1
+  shift
   _JIRA_FIELD_VALUE=$1
   cat <<POST_DATA_EOF
   {
     "fields":
         {
-            "customfield_14600": [{"name": "$_JIRA_FIELD_VALUE"}]
+            "${_JIRA_FIELD_ID}": [{"name": "$_JIRA_FIELD_VALUE"}]
         }
   }
 POST_DATA_EOF
 }
+
+
+#
+# Function
+#
+generatePostDataToUpdateEpicLink() {
+  _JIRA_FIELD_ID=$1
+  shift
+  _JIRA_FIELD_VALUE=$1
+  cat <<POST_DATA_EOF
+  {
+    "fields":
+        {
+            "${_JIRA_FIELD_ID}": "$_JIRA_FIELD_VALUE"
+        }
+  }
+POST_DATA_EOF
+}
+
 
 #
 # Function
@@ -171,9 +194,26 @@ set_watchers_group_to_jira_ticket() {
 
     for group in ${watchersGroupArray[@]}; do
         echo "Adding $group to watcher group"
-        jira_put_call $REST_API_PATH generatePostDataToUpdateWatcherGroupField $group
+        jira_put_call $REST_API_PATH generatePostDataToUpdateWatcherGroupField customfield_14600 $group
     done
 }
+
+
+#
+# Function
+#
+set_epic_link_to_jira_ticket_if_JIRA_EPIC_LINK() {
+    REST_API_PATH="issue/$JIRA_ID"
+    epic_link=($JIRA_EPIC_LINK)
+
+    if [ ! -z ${JIRA_EPIC_LINK} ]; then
+        echo "Setting Epic Link to $JIRA_EPIC_LINK"
+        jira_put_call $REST_API_PATH generatePostDataToUpdateEpicLink customfield_10001 $JIRA_EPIC_LINK
+    else
+        echo "Skipping the Epic Link value setting as it is empty"
+    fi
+}
+
 
 #
 # Main start here
@@ -209,3 +249,6 @@ set_labels_id_to_jira_ticket
 
 # set the watchers groups
 set_watchers_group_to_jira_ticket
+
+# set the epic link
+set_epic_link_to_jira_ticket_if_JIRA_EPIC_LINK
