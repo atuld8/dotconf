6a26953694270f0bb5ea9f58dcec6431a0dc9790
Atul Das <Atul.Das1@Veritas.com>
2024-10-08 17:30:53 +0530

    fix: Add custom resolution setting through the script

--- jira/j.updateStatusDefault2Close.py
+++ jira/j.updateStatusDefault2Close.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3.12
+
+'''
+Fixed               A fix for this issue is checked into the tree and tested.
+Can't Fix           This is a problem for which there is no known fix.
+Won't Fix           The problem described is an issue which will never be fixed.i
+Duplicate           The problem is a duplicate of an existing issue.
+Incomplete          The problem is not completely described.
+Cannot Reproduce    All attempts at reproducing this issue failed, or not enough information was available to reproduce the issue.
+                    Reading the code produces no clues as to why this behavior would occur.
+                    If more information appears later, please reopen the issue.
+Done                GreenHopper Managed Resolution
+Rejected            Not accepted into the backlog; will not be considered
+Won't Do            This issue won't be actioned.
+Monitored           Monitored
+Not an Issue        Not an Issue
+'''
+
 import json
 import os
 import argparse
@@ -70,18 +87,30 @@ def get_status_id(ticket_id, status):
 
 
 # Function to update status for the Jira issue
-def update_status(ticket_id, status_id):
+def update_status(ticket_id, status_id, resolution="Fixed"):
     """
     set the transition id to jira
     """
 
     url = f"{issue_url}/{ticket_id}/transitions"
 
-    payload = json.dumps({
-        "transition": {
-            "id": status_id
-        }
-    })
+    if resolution == "":
+        payload = json.dumps({
+            "transition": {
+                "id": status_id
+            }
+        })
+    else:
+        payload = json.dumps({
+            "transition": {
+                "id": status_id
+            },
+            "fields": {
+                "resolution": {
+                    "name": resolution
+                }
+            }
+        })
 
     response = requests.post(url, headers=headers, data=payload, timeout=20)
 
@@ -92,7 +121,7 @@ def update_status(ticket_id, status_id):
 
 
 # Main function to update the status
-def main(ticket_id, status):
+def main(ticket_id, status, resolution):
 
     """
     Update the status of jira ticket id to status
@@ -109,7 +138,7 @@ def main(ticket_id, status):
 
         # Update Jira ticket with new status
         print(f"Setting the status id to {status_id}")
-        update_status(ticket_id, status_id)
+        update_status(ticket_id, status_id, "")
 
     status_id = get_status_id(ticket_id, status)
     if status_id is None:
@@ -118,7 +147,10 @@ def main(ticket_id, status):
 
     # Update Jira ticket with new status
     print(f"Setting the status id to {status_id}")
-    update_status(ticket_id, status_id)
+    if status in ("Done", "Closed"):
+        update_status(ticket_id, status_id, resolution)
+    else:
+        update_status(ticket_id, status_id, "")
 
 
 # Command-line argument parser
@@ -126,8 +158,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Update labels for a Jira ticket")
     parser.add_argument("ticket_id", help="Jira ticket ID (e.g., JIR-123)")
     parser.add_argument("--status", default="Close", help="The status value. Default is Closed.")
+    parser.add_argument("--resolution", default="Fixed", help="The resolution value. Default is Fixed.")
 
     args = parser.parse_args()
 
     # Perform label management
-    main(args.ticket_id, args.status)
+    main(args.ticket_id, args.status, args.resolution)
