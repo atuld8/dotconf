f5ecfce2c351179fab5c41284a711ea327a9c3fe
Atul Das <Atul.Das1@Veritas.com>
2024-12-16 15:49:30 +0530

    Add support to update the Epic link

---
+++ jira/j.setEpicLink.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+#
+
+#Set below env before using it
+#export JIRA_ACC_TOKEN='tkn'
+#export JIRA_SERVER_NAME=".com"
+#export JIRA_PROJECT_NAME="PROJ"
+#export JIRA_EPIC_LINK="EPIC-1234"
+
+# Arguments
+JIRA_ID=$1
+NEW_EPIC_ID=$2
+
+
+#
+# Function
+#
+generatePostDataToUpdateEpicLink() {
+  _JIRA_FIELD_ID=$1
+  shift
+  _JIRA_FIELD_VALUE=$1
+  cat <<POST_DATA_EOF
+  {
+    "fields":
+        {
+            "${_JIRA_FIELD_ID}": "$_JIRA_FIELD_VALUE"
+        }
+  }
+POST_DATA_EOF
+}
+
+
+#
+# Function
+#
+jira_get_call() {
+   REST_API_PATH=$1;
+   shift;
+   curl --silent -H "Authorization: Bearer $JIRA_ACC_TOKEN" "https://$JIRA_SERVER_NAME/rest/api/2/$REST_API_PATH" $@;
+}
+
+#
+# Function
+#
+jira_put_call() {
+   REST_API_PATH=$1;
+   shift;
+   REST_GET_DATA_FUNC=$@;
+
+   curl --silent -X PUT \
+        -H "Authorization: Bearer $JIRA_ACC_TOKEN" \
+        -H "Content-Type:application/json" \
+        "https://$JIRA_SERVER_NAME/rest/api/2/$REST_API_PATH" \
+        -d "$($REST_GET_DATA_FUNC)"
+}
+
+#
+# Function
+#
+jira_post_call() {
+   REST_API_PATH=$1;
+   shift;
+   REST_GET_DATA_FUNC=$@;
+
+   echo "curl -D- -X POST \
+        -H \"Authorization: Bearer $JIRA_ACC_TOKEN\" \
+        -H \"Content-Type:application/json\" \
+        \"https://$JIRA_SERVER_NAME/rest/api/2/$REST_API_PATH\" \
+        --data-raw '"$($REST_GET_DATA_FUNC)"'" | tee sh
+}
+
+
+#
+# Function
+#
+set_epic_link_to_jira_ticket() {
+    REST_API_PATH="issue/$JIRA_ID"
+    epic_link=($NEW_EPIC_ID)
+
+    if [ ! -z ${NEW_EPIC_ID} ]; then
+        echo "Setting Epic Link to $NEW_EPIC_ID"
+        jira_put_call $REST_API_PATH generatePostDataToUpdateEpicLink customfield_10001 $NEW_EPIC_ID
+    else
+        echo "Skipping the Epic Link value setting as it is empty"
+    fi
+}
+
+
+#
+# Main start here
+#
+if [ "$JIRA_ACC_TOKEN" == "" ]; then
+    echo "JIRA_ACC_TOKEN not defined"
+    exit 1
+fi
+
+if [ "$JIRA_SERVER_NAME" == "" ]; then
+    echo "JIRA_SERVER_NAME not defined"
+    exit 1
+fi
+
+
+if [ "$JIRA_ID" == "" ]; then
+    echo "Please pass the JIRA_ID with this script"
+    exit 1
+fi
+
+if [ "$NEW_EPIC_ID" == "" ]; then
+    echo "Please pass the EPIC_ID with this script"
+    exit 1
+fi
+
+
+# set the epic link
+set_epic_link_to_jira_ticket
