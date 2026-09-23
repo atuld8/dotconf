bb2bd073190eba89fbbd039f8414a24eb8bc0047
atuld8 <atuld8@gmail.com>
2026-09-23 16:20:34 +0530

    Load plugin if ansi ext found

--- nvim/.config/nvim/lua/plugins/baleia.lua
+++ nvim/.config/nvim/lua/plugins/baleia.lua
@@ -7,14 +7,27 @@ return {
   config = function()
     local baleia = require("baleia").setup()
 
-    -- Rendering is explicit so opening a file never changes its buffer.
-    vim.api.nvim_create_user_command("BaleiaColorize", function()
-      local bufnr = vim.api.nvim_get_current_buf()
+    local function colorize_buffer(bufnr)
       if vim.bo[bufnr].buftype ~= "" or not vim.bo[bufnr].modifiable then
-        vim.notify("Baleia: current buffer is not modifiable", vim.log.levels.WARN)
-        return
+        return false
       end
       baleia.once(bufnr)
+      return true
+    end
+
+    -- Automatically render ANSI files after they are read.
+    vim.api.nvim_create_autocmd("BufReadPost", {
+      pattern = "*.ansi",
+      callback = function(args)
+        colorize_buffer(args.buf)
+      end,
+    })
+
+    -- Manual command remains available for any modifiable file.
+    vim.api.nvim_create_user_command("BaleiaColorize", function()
+      if not colorize_buffer(vim.api.nvim_get_current_buf()) then
+        vim.notify("Baleia: current buffer is not modifiable", vim.log.levels.WARN)
+      end
     end, {})
   end,
 }
